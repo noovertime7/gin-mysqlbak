fab2a8aeb4251ca99b96c3f2f627ff9cbe9dacbc
chenteng <cc17854330572@163.com>
2022-08-23 08:41:22 +0800

    fix: 修复首页大屏barchart显示bug

--- agent/agentcontroller/dashboard.go
+++ agent/agentcontroller/dashboard.go
@@ -1,15 +1,9 @@
 package agentcontroller
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/micro/go-micro/v2/client"
 	"github.com/noovertime7/gin-mysqlbak/agent/agentdto"
-	"github.com/noovertime7/gin-mysqlbak/agent/pkg"
-	"github.com/noovertime7/gin-mysqlbak/agent/proto/host"
 	"github.com/noovertime7/gin-mysqlbak/middleware"
-	"github.com/noovertime7/gin-mysqlbak/public"
-	"github.com/noovertime7/mysqlbak/pkg/log"
 )
 
 type DashBoardController struct{}
@@ -25,35 +19,15 @@ func (d *DashBoardController) GetBarChartData(ctx *gin.Context) {
 		PageNo:   1,
 		PageSize: 999,
 	})
-	var num = 0
 	var name []string
 	var taskNum []int64
-	for _, service := range serviceList.AgentOutPutItem {
-		hostService, addr, err := pkg.GetHostService(service.ServiceName)
-		if err != nil {
-			log.Logger.Error("获取Agent地址失败", err)
-			middleware.ResponseError(ctx, public.ParamsBindErrorCode, err)
-			return
-		}
-		var ops client.CallOption = func(options *client.CallOptions) {
-			options.Address = []string{addr}
-		}
-		out, err := hostService.GetHostList(ctx, &host.HostListInput{
-			Info:     "",
-			PageNo:   1,
-			PageSize: 999,
-		}, ops)
-		if err != nil {
-			middleware.ResponseError(ctx, public.ParamsBindErrorCode, err)
-			return
-		}
-		for _, hostItem := range out.ListItem {
-			num = num + int(hostItem.TaskNum)
-			fmt.Println(service.ServiceName, hostItem.TaskNum)
-		}
-		name = append(name, service.ServiceName)
-		taskNum = append(taskNum, int64(num))
+	for _, s := range serviceList.AgentOutPutItem {
+		name = append(name, s.ServiceName)
+		taskNum = append(taskNum, int64(s.TaskNum))
 	}
-	out := agentdto.BarChartOutPut{ServiceName: name, TaskNum: taskNum}
+	out := &agentdto.BarChartOutPut{
+		ServiceName: name,
+		TaskNum:     taskNum,
+	}
 	middleware.ResponseSuccess(ctx, out)
 }
